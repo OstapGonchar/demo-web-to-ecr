66e7f9b3f95ae4a57218325e39e2dff8bd21ae3f
Ostap Gonchar <ostap.gonchar@gmail.com>
2022-02-13 22:57:33 +0100

    Added CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.6.10"
 	kotlin("plugin.spring") version "1.6.10"
+	id("com.gorylenko.gradle-git-properties") version "1.5.1"
 }
 
 group = "com.web"
@@ -33,4 +34,8 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+springBoot {
+	buildInfo()
 }
